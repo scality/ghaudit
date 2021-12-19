5b12d6e8286509284461e9eb3bd64301f28eaa72
Kevin Gruber <kevin.gruber@scality.com>
2021-12-19 20:46:51 +0100

    migrated tests
    launch tests from tox
    migrated tests from the policy module

--- src/ghaudit/policy.py
+++ src/ghaudit/policy.py
@@ -734,24 +734,3 @@ def user_perm(
 
 def repo_visibility(policy: Policy, repo_name: str) -> Visibility:
     return policy.repo_visibility(repo_name)
-
-
-def test() -> None:
-    assert not perm_higher("admin", "admin")
-    assert perm_higher("admin", "write")
-    assert perm_higher("admin", "read")
-    assert not perm_higher("write", "admin")
-    assert not perm_higher("write", "write")
-    assert perm_higher("write", "read")
-    assert not perm_higher("read", "admin")
-    assert not perm_higher("read", "write")
-    assert not perm_higher("read", "read")
-    assert perm_highest("admin", "admin") == "admin"
-    assert perm_highest("admin", "write") == "admin"
-    assert perm_highest("admin", "read") == "admin"
-    assert perm_highest("write", "admin") == "admin"
-    assert perm_highest("write", "write") == "write"
-    assert perm_highest("write", "read") == "write"
-    assert perm_highest("read", "admin") == "admin"
-    assert perm_highest("read", "write") == "write"
-    assert perm_highest("read", "read") == "read"
