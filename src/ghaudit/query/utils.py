4cb06e2eec5b0cd8394e57d2cf598c899fbee3ae
igor vaslyuk <igor.vaslyuk@scality.com>
2021-12-23 15:34:44 +0100

    ISMS-575 refactor jinja template load
    - refactor of jinja temlate load

--- src/ghaudit/query/utils.py
+++ src/ghaudit/query/utils.py
@@ -1,4 +1,7 @@
+from pathlib import Path
 from typing import Optional, TypedDict
+import sys
+import jinja2
 
 
 class PageInfo(TypedDict):
@@ -10,3 +13,14 @@ def page_info_continue(page_infos: Optional[PageInfo]) -> bool:
     if not page_infos or page_infos["hasNextPage"]:
         return True
     return False
+
+
+def get_template_dir():
+    return Path(sys.prefix) / "share" / "ghaudit" / "fragments"
+
+
+def jinja_env():
+    return jinja2.Environment(
+        loader=jinja2.FileSystemLoader(get_template_dir()),
+        undefined=jinja2.StrictUndefined,
+    )
