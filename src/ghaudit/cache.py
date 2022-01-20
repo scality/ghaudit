d50349a95ad0fc7c5a0876ced0ccbf613e861522
Kevin Gruber <kevin.gruber@scality.com>
2022-01-20 02:39:51 +0100

    cache: docstrings, typing, lint

--- src/ghaudit/cache.py
+++ src/ghaudit/cache.py
@@ -1,9 +1,15 @@
+"""Github remote state synchronisation with a local storage (hence cache)."""
+
+from __future__ import annotations
+
 import json
 import tempfile
 from os import environ, fsync, makedirs, path, rename
 from pathlib import Path
+from typing import Dict, List
 
-from ghaudit import schema
+from ghaudit import auth, schema
+from ghaudit.config import Config
 from ghaudit.query.branch_protection_push_allowances import (
     BranchProtectionPushAllowances,
 )
@@ -19,25 +25,29 @@ from ghaudit.query.user import UserQuery
 from ghaudit.query.user_role import TeamMemberQuery
 
 
-def file_path():
-    def parent_dir():
-        if environ.get("XDG_DATA_HOME"):
-            return Path(environ.get("XDG_DATA_HOME"))
-        if environ.get("HOME"):
-            return Path(environ.get("HOME")) / ".local" / "share"
+def file_path() -> Path:
+    def parent_dir() -> Path:
+        xdg_data_home = environ.get("XDG_DATA_HOME")
+        if xdg_data_home:
+            return Path(xdg_data_home)
+        home = environ.get("HOME")
+        if home:
+            return Path(home) / ".local" / "share"
         return Path("/")
 
     return parent_dir() / "ghaudit" / "compliance" / "cache.json"
 
 
 def load() -> schema.Rstate:
+    """Load remote state from cache file."""
     with open(file_path(), encoding="UTF-8") as cache_file:
         rstate = json.load(cache_file)
         schema.validate(rstate)
         return rstate
 
 
-def store(data):
+def store(data: schema.Rstate) -> None:
+    """Store remote state to file."""
     ofilepath = file_path()
     if not path.exists(ofilepath.parent):
         makedirs(ofilepath.parent)
@@ -52,7 +62,8 @@ def store(data):
         fsync(cache_file.fileno())
 
 
-def refresh(config, auth_driver):
+def refresh(config: Config, auth_driver: auth.AuthDriver) -> None:
+    """Refresh the remote state from github to a local file."""
     data = _sync(config, auth_driver)
     print("validating cache")
     if schema.validate(data):
@@ -84,14 +95,14 @@ def _sync_progress(data, query):
     print("<<<<<<<<<<<<<<<<<<<<<<<<<<<<")
 
 
-def _sync(config, auth_driver):
+def _sync(config: Config, auth_driver):
     data = schema.empty()
     found = {
         "teams": [],
         "repositories": [],
         "collaborators": [],
         "bprules": [],
-    }
+    }  # type: Dict[str, List[str]]
     workaround2 = {"team": 0, "repo": 0, "user": 0, "bprules": 0}
     query = CompoundQuery(MAX_PARALLEL_QUERIES)
     demo_params = {
@@ -99,7 +110,7 @@ def _sync(config, auth_driver):
         "teamsMax": ORG_TEAMS_MAX,
         "membersWithRoleMax": ORG_MEMBERS_MAX,
         "repositoriesMax": ORG_REPOSITORIES_MAX,
-    }
+    }  # type: Dict[str, str | int]
 
     query.add_frag(FRAG_PAGEINFO_FIELDS)
     query.append(OrgTeamsQuery())
@@ -162,11 +173,11 @@ def _sync(config, auth_driver):
         for rule_id in new_bp_rules:
             query.append(
                 BranchProtectionPushAllowances(
-                    rule_id, workaround2["bprules"], 10
+                    str(rule_id), workaround2["bprules"], 10
                 )
             )
             workaround2["bprules"] += 1
-            found["bprules"].append(rule_id)
+            found["bprules"].append(str(rule_id))
 
         _sync_progress(data, query)
 
