8cb245a534f60f0f31e1e7c0d0230b7b25985a6f
Kevin Gruber <kevin.gruber@scality.com>
2022-01-20 02:24:14 +0100

    auth: docstrings

--- src/ghaudit/auth.py
+++ src/ghaudit/auth.py
@@ -1,3 +1,5 @@
+"""Authentication drivers."""
+
 from typing import Callable, Mapping
 
 import passpy.store
@@ -6,6 +8,12 @@ AuthDriver = Callable[[], Mapping[str, str]]
 
 
 def github_auth_token_passpy(path: str) -> AuthDriver:
+    """Return a pass auth driver.
+
+    Return a callback providing token authentication to github, given the path
+    of a secret in pass, the GnuPG-based password manager.
+    """
+
     def get_token(path: str) -> str:
         return passpy.store.Store().get_key(path).strip()
 
